d4eca1b80b4432a8c5736209234db362d8059f3a
Arijit Datta from Kolkata <arijitdatta.kolkata@gmail.com>
2025-01-28 20:57:53 +0530

    Language Switching Works

--- iabrf-fe/MyReadMe.txt
+++ iabrf-fe/MyReadMe.txt
@@ -1,2 +1,2 @@
 For translations
-https://medium.com/@shubham3480/adding-multi-lingual-support-in-react-44fa34a9500c#:~:text=To%20add%20multi%2Dlanguage%20support,React%2C%20Vue%2C%20etc.)
+    https://lokalise.com/blog/react-i18n-intl/
---
+++ iabrf-fe/src/components/Wrapper.tsx
@@ -0,0 +1,34 @@
+import {createContext, useState} from 'react';
+import {IntlProvider} from 'react-intl';
+import English from '../assets/translations/en.json'
+import Bengali from '../assets/translations/bn.json'
+export const Context = createContext()
+const local = navigator.language;
+
+let lang;
+if (local==="bn") {
+   lang = Bengali;
+} else {
+  lang = English;
+}
+const Wrapper = (props:any) => {
+   const [locale, setLocale] = useState(local);
+   const [messages, setMessages] = useState(lang);
+   function selectLanguage(e:any) {
+       const newLocale = e.target.value;
+       setLocale(newLocale);
+       if (newLocale === 'bn') {
+           setMessages(Bengali);
+       } else {
+          setMessages(English)
+       }
+   }
+   return (
+       <Context.Provider value = {{locale, selectLanguage}}>
+           <IntlProvider messages={messages} locale={locale}>
+               {props.children}
+           </IntlProvider>
+       </Context.Provider>
+   );
+}
+export default Wrapper;
